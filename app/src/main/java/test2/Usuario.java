407a9fe9b86f09c17b263f8337e5b13d08e92584
MDCpedro <pedromdc101@gmail.com>
2024-03-20 18:17:05 +0100

    guardado

--- app/src/main/java/test2/Usuario.java
+++ app/src/main/java/test2/Usuario.java
@@ -1,11 +1,15 @@
 package test2;
+
 public class Usuario {
     private String nom_usuari; 
     private String correu_electronic;
+    private int ID;
 
-    public Usuario(String nom_usuari, String correu_electronic) {
+
+    public Usuario(String nom_usuari, String correu_electronic, int ID) {
         this.nom_usuari = nom_usuari;
         this.correu_electronic = correu_electronic;
+        this.ID = ID;
     }
     
 }
